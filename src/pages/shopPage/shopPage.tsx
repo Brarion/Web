56a17656c9fcd0f987958123b7db251a32dbb251
Brarion <pr.nick126@gmail.com>
2020-11-20 23:37:53 +0300

    Лабораторная №2. Фикс

--- src/pages/shopPage/shopPage.tsx
+++ src/pages/shopPage/shopPage.tsx
@@ -6,27 +6,6 @@ import Card from "react-bootstrap/Card";
 
 import s from "./style.module.scss";
 
-/*
-<div class="card">
-      <img class="card-img-top" src="../../../public/games/MyTimeAtPortia.jpg" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">My Time At Portia</h5>
-        <p class="card-text">Начните новую жизнь в очаровательном городе Портии! Восстановите мастерскую своего отца и
-          верните ей былую славу!</p>
-      </div>
-      <div class="card-body">
-        <span>
-          <p>Разработчик: </p>
-          <a href="#" class="card-link">Pathea Games</a>
-        </span>
-        <span>
-          <p>Издатель: </p>
-          <a href="#" class="card-link">Team17 Digital Ltd</a>
-        </span>
-      </div>
-    </div>
- */
-
 const ShopPage = () => {
   return (
     <div className={s.wrapper}>
